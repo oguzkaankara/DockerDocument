fe5caf9faf4ae56730715bc8c9dc63fd3c86a744
Oğuzhan kara <60827737+oguzkaankara@users.noreply.github.com>
2023-05-20 00:52:19 +0300

    doker24

--- DockerApi/Program.cs
+++ DockerApi/Program.cs
@@ -10,11 +10,14 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+
 
 app.UseHttpsRedirection();
 
